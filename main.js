2eeaaff7175803452f67f258b52ba24a927883b9
Rusich94 <92531400+Rusich94@users.noreply.github.com>
2021-10-27 19:45:47 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+//const
+const SEX = 'male';
+const EYE_COLOR = 'green';
+const SKIN_COLOR = 'white';
+const BlOOD_TYPE = 3;
+const NAME = 'Ruslan';
+const SURNAME = 'Kolomiets';
+const WIFE = 'Anna';
+const NATIONALITY = 'ukrainian';
+const VEGETARIANISM = false;
+
+//let
+let age = 27;
+let height = 178;
+let weight = 80;
+let shoe_size = 43;
+let clothing_size = 'L';
+let hair_lenght = 'short';
+let character = undefined;
+let job = 'Skyeng';
+let citizenship = 'Ukraine';
+let favorit_dish = null;
+let favorit_music = undefined;
+let kindness = true;
+
+//console_const
+const type_of_SEX = typeof(SEX);
+console.log(SEX, type_of_SEX);
+const type_of_EYE_COLOR = typeof(EYE_COLOR);
+console.log(EYE_COLOR, type_of_EYE_COLOR);
+const type_of_SKIN_COLOR = typeof(SKIN_COLOR);
+console.log(SKIN_COLOR, type_of_SKIN_COLOR);
+const type_of_NAME = typeof(NAME);
+console.log(NAME, type_of_NAME);
+const type_of_SURNAME = typeof(SURNAME);
+console.log(SURNAME, type_of_SURNAME);
+const type_of_WIFE = typeof(WIFE);
+console.log(WIFE, type_of_WIFE);
+const type_of_NATIONALITY = typeof(NATIONALITY);
+console.log(NATIONALITY, type_of_NATIONALITY);
+const type_of_VEGETARIANISM = typeof(VEGETARIANISM);
+console.log(VEGETARIANISM, type_of_VEGETARIANISM);
+
+//console_let
+let type_of_age = typeof(age);
+console.log(age, type_of_age);
+let type_of_height = typeof(height);
+console.log(height, type_of_height);
+let type_of_weight = typeof(weight);
+console.log(weight, type_of_weight);
+let type_of_shoe_size = typeof(shoe_size);
+console.log(shoe_size, type_of_shoe_size);
+let type_of_clothing_size = typeof(clothing_size);
+console.log(clothing_size, type_of_clothing_size);
+let type_of_hair_lenght = typeof(hair_lenght);
+console.log(hair_lenght, type_of_hair_lenght);
+let type_of_character = typeof(character);
+console.log(character, type_of_character);
+let type_of_job = typeof(job);
+console.log(job, type_of_job);
+let type_of_citizenship = typeof(citizenship);
+console.log(citizenship, type_of_citizenship);
+let type_of_favorit_dish = typeof(favorit_dish);
+console.log(favorit_dish, type_of_favorit_dish);
+let type_of_kindness = typeof(kindness);
+console.log(kindness, type_of_kindness);
+
+//alert
+let type_of_favorit_music = typeof(favorit_music);
+alert('favorit_music, type_of_favorit_music');
